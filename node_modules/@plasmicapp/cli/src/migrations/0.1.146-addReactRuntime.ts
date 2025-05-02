9b9e86e58df74d0d5df01cc1ad13af0d17687cc6
phucvarmeta <phucth@var-meta.com>
2025-05-02 23:10:27 +0700

    fix: dependencies

--- node_modules/@plasmicapp/cli/src/migrations/0.1.146-addReactRuntime.ts
+++
@@ -1,8 +0,0 @@
-import { PlasmicConfig } from "../utils/config-utils";
-
-export function ensureReactRuntime(config: PlasmicConfig) {
-  if (!config.code.reactRuntime) {
-    config.code.reactRuntime = "classic";
-  }
-  return config;
-}
